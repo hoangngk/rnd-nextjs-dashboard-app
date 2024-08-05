94bfc4f5fffb329150150c2bb289dbe5d41f839a
Hoang NK <n.khanhhoang@gmail.com>
2024-08-05 14:38:44 +0700

    Init app

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InvoicesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Invoices Page</h1>
+      {/* Add your invoices content here */}
+    </div>
+  );
+};
+
+export default InvoicesPage;

32d610aab89f5f497a6b1a685426e47c2da63429
Hoang NK <n.khanhhoang@gmail.com>
2024-08-14 15:44:53 +0700

    fix

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,9 +1,7 @@
-import { fetchCustomers } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import Form from '@/app/ui/invoices/create-form';
 
 export default async function Page() {
-  const customers = await fetchCustomers();
+  // const customers = await fetchCustomers();
   
   return (
     <main>
@@ -17,7 +15,7 @@ export default async function Page() {
           },
         ]}
       />
-        <Form customers={customers} />
+        {/* <Form customers={customers} /> */}
     </main>
   );
 }
